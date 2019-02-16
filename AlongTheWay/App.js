b96d0b63a55d5c2800c44e6aea066a79d65d3ee2
sarah <sbroom7@lsu.edu>
2019-02-16 14:47:41 -0600

    Update App.js

--- AlongTheWay/App.js
+++ AlongTheWay/App.js
@@ -8,11 +8,34 @@
  */
 
 import React, {Component} from 'react';
-import { Text, View,StyleSheet, KeyboardAvoidingView,Keyboard} from 'react-native';
+import { Text, View,StyleSheet, KeyboardAvoidingView,Keyboard, Platform, PermissionsAndroid, Alert} from 'react-native';
 import ControlBar from './basicapp/Components/ControlBar';
 import MapGui from './basicapp/Components/MapGui';
 import Main from './basicapp/Components/Main';
 
+export async function request_location_runtime_permission(){
+
+    try{
+        const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+            'title': 'ReactNativeCode Location Permission',
+            'message': 'ReactNativeCode App needs access to your location '
+        }
+       )
+       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+
+        Alert.alert("Location Permission Granted.");
+       }
+       else {
+
+        Alert.alert("Location Permission Not Granted");
+
+       }
+    } catch (err){
+      console.warn(err)
+    }
+}
 
 export default class App extends Component {
 
@@ -26,7 +49,34 @@ export default class App extends Component {
 
       }
 
+      async componentDidMount(){
+              await request_location_runtime_permission()
+
+          }
+
+         /* render() {
+            return(
+                <View style={styles.MainContainer}>
+
+                    <Text>React Native Runtime Permission Request</Text>
+
+                </View>
+            );
+
+      }*/
 }
+
+const styles = StyleSheet.create({
+    MainContainer:{
+    flex:1,
+    paddingTop: (Platform.OS) === 'ios' ? 20:0,
+    justifyContent: 'center',
+    margin: 20
+    }
+    });
+
+
+
 
 
 
