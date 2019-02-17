d66d4d936b6372287840fda4a7ecc38842cbfb00
zang227 <159381Zeringue@chsbr.net>
2019-02-17 09:31:50 -0600

    Create SearchBar.js

---
+++ AlongTheWay/basicapp/Components/SearchBar.js
@@ -0,0 +1,128 @@
+import React from 'react';
+
+import { SearchBar } from 'react-native-elements';
+
+import { Text, View,StyleSheet, KeyboardAvoidingView,Keyboard, TextInput, ScrollView, ActivityIndicator} from 'react-native';
+
+import { GoogleAutoComplete } from 'react-native-google-autocomplete';
+
+import LocationItem from '../Components/LocationItem';
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default class App extends React.Component {
+
+  render() {
+
+    return (
+
+      <View style={styles.container}>
+
+        <GoogleAutoComplete apiKey =  {'AIzaSyCvfftvHMnURvTGkaiVyHQMdcYsGZsCVNs'} debounce = {500} minLength ={2}>
+
+
+
+            {({handleTextChange,
+
+               locationResults,
+
+               fetchDetails,
+
+               isSearching
+
+               }) => (
+
+                <React.Fragment>
+
+                    {console.log('locationResults', locationResults)}
+
+                  <View style = {styles.inputWrapper}>
+
+                   <TextInput
+
+                    style={styles.textInput}
+
+                    placeholder="Type Here..."
+
+                    onChangeText ={handleTextChange}
+
+
+
+                    />
+
+
+                        </View>
+
+                        <ScrollView>
+
+                        {locationResults.map(el => (
+
+                            <LocationItem
+
+                                {...el}
+
+                                key={el.id}
+
+                                />
+
+                            ))}
+
+                        </ScrollView>
+
+                            </React.Fragment>
+
+                    )}
+
+        </GoogleAutoComplete>
+
+      </View>
+
+    );
+
+  }
+
+}
+
+
+
+const styles = StyleSheet.create({
+
+    container: {
+
+        backgroundColor: 'white',
+
+        alignItems: 'center',
+
+        justifyContent: 'center',
+
+    },
+
+    textInput: {
+
+        height: 40,
+
+        width: 400,
+
+        borderWidth: 1,
+
+        paddingHorizontal: 16
+
+    },
+
+
+
+
+
+
+});
+
