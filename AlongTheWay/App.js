db6fda5c9a7d7edf95a7a02a6bc5325719141aeb
avery <sfost13@lsu.edu>
2019-02-19 11:59:34 -0600

    Connect Options page to App

--- AlongTheWay/App.js
+++ AlongTheWay/App.js
@@ -1,10 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
 import { Text, View,StyleSheet, KeyboardAvoidingView,Keyboard} from 'react-native';
 import ControlBar from './basicapp/Components/ControlBar';
 import MapGui from './basicapp/Components/MapGui';
 import Main from './basicapp/Components/Main';
 import SearchBar from './basicapp/Components/SearchBar';
 import {Platform, PermissionsAndroid, Alert} from 'react-native';
-import React, {Component} from 'react';
+import Options from './basicapp/Components/Option'
+import ReviewForm from './basicapp/Components/ReviewForm';
 
 
 export async function request_location_runtime_permission() {
@@ -17,7 +28,6 @@ try {
       }
     )
     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-
 
      // Alert.alert("Location Permission Granted.");
     }
@@ -33,39 +43,82 @@ try {
 
 
 }
+
 export default class App extends Component {
+
+         async componentDidMount() {
+
+          await request_location_runtime_permission()
+
+        }
 
      constructor(props){
      super(props);
      this.state = {
         isSearching : false,
-        //optionsClicked: false,
+        radius: 1000,
+        isReviewing: false,
+        inOptions: false,
+        reviews: [],
+        mapsType: 'standard'
      }
 
      }
+
+        inOptions(){
+        this.setState( previousState => ( {inOptions: !previousState.inOptions} ) );
+        }
 
        onSearch(){
-       this.setState(previousState => (
+       this.setState( previousState => ( {isSearching: !previousState.isSearching} ) );
+       }
 
-                     {isSearching: !previousState.isSearching }
-                                                                 )
-                             );
+       onRadiusChange(radius){
+        this.setState({radius: radius});
+        console.log(this.state.radius);
+       }
+
+       onReview(){
+       this.setState( previousState => ( {isReviewing: !previousState.isReviewing} ) );
 
        }
-     async componentDidMount() {
 
-    await request_location_runtime_permission()
+       addReview(userReview){
 
-  }
+         this.state.reviews.push(userReview);
+          console.log(this.state.reviews);
+          this.onReview();
+         }
 
 
       render(){
 
+
+
      {
-          if(!this.state.isSearching)
-          {return( <Main onSearch = {this.onSearch.bind(this)}/> );}
+           if(this.state.inOptions)
+           {
+            return( <Options onRadiusChange = {(radius) => {this.onRadiusChange(radius)}}
+                            radius = {this.state.radius} inOptions = {this.inOptions.bind(this)} />)
+
+           }
+
+          if(this.state.isReviewing)
+          {
+             return( <ReviewForm addReview = {(userReview) => {this.addReview(userReview)}}/> );
+          }
+
+          if(this.state.isSearching)
+          {
+             return( <SearchBar/> );
+          }
+
           else
-          {return( <SearchBar/> );}
+          {
+            return( <Main onSearch = {this.onSearch.bind(this)} onReview = {this.onReview.bind(this)}
+                                     inOptions = {this.inOptions.bind(this)} radius = {this.state.radius}
+                                     onReview = {this.onReview.bind(this)}/> );
+          }
      }
 
       }
@@ -75,9 +128,4 @@ export default class App extends Component {
 
 
 
-
-
-
-
-    
 
