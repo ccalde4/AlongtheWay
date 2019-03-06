1f47d2bc3b0b21d9c5fd1532af56a50dd8058215
Clauida Caldera <ccalde4@lsu.edu>
2019-03-06 08:14:14 -0600

    Clean up ReviewForm

--- AlongTheWay/basicapp/Components/ReviewForm.js
+++ AlongTheWay/basicapp/Components/ReviewForm.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Text,TextInput, View,ScrollView, StyleSheet,Keyboard, Dimensions, TouchableOpacity,TouchableWithoutFeedback,KeyboardAvoidingView} from 'react-native';
-const { width, height } = Dimensions.get('window');
-
+import StarRating from 'react-native-star-rating';
+var { winHeight, winWidth } = Dimensions.get('window');
 
   export default class ReviewForm extends Component{
 
@@ -9,166 +9,165 @@ const { width, height } = Dimensions.get('window');
      super(props);
 
      this.state = {
-         rating: 1,
-         text : ''
+
+         starCount: 1,
+         text : '',
 
      }
 }
    componentWillUnmount(){
            Keyboard.dismiss()
    }
-
-    onOneClick(){this.setState({rating:1})}
-    onTwoClick(){this.setState({rating:2})}
-    onThreeClick(){this.setState({rating:3})}
-    onFourClick(){this.setState({rating:4})}
-    onFiveClick(){this.setState({rating:5})}
-
-    onEnterClick(){
-
-      let userReview =
-      {
-        comment: this.state.text,
-        rating: this.state.rating,
-      }
-      this.props.addReview(userReview);
-
-     }
-
-
-
-      render(){
-
-      return(
-
-
-          <View style = {styles.Gui}   >
-
-                    <Text> Leave A Rating                             {this.state.rating} </Text>
-
-                <View style = {styles.layout2}>
-                  <View style = {styles.row2} >
-
-                      <TouchableOpacity onPress = {this.onOneClick.bind(this)}>
-                       <View style = {styles.button1} />
-                      </TouchableOpacity>
-
-                      <TouchableOpacity onPress = {this.onTwoClick.bind(this)}>
-                        <View style = {styles.button1} />
-                      </TouchableOpacity>
-
-                      <TouchableOpacity onPress = {this.onThreeClick.bind(this)}>
-                        <View style = {styles.button1} />
-                      </TouchableOpacity>
-
-                       <TouchableOpacity onPress = {this.onFourClick.bind(this)}>
-                         <View style = {styles.button1} />
-                       </TouchableOpacity>
-
-                       <TouchableOpacity onPress = {this.onFiveClick.bind(this)}>
-                         <View style = {styles.button1} />
-                       </TouchableOpacity>
-
-                     </View>
-                 </View>
-                   <ScrollView  keyboardShouldPersistTaps = {"never"} style = {styles.textIn}>
-                  <KeyboardAvoidingView  behavior = "padding" enabled>
-                           <View >
-                             <TextInput  placeholder = {'Leave a comment'}  onChangeText={(text) => this.setState({text})}
-                               value = {this.state.text}/>
-                            </View>
-                   </KeyboardAvoidingView>
-                    </ScrollView>
-
-                   <TouchableOpacity style = {styles.enter} onPress = {this.onEnterClick.bind(this)}>
-                              <View>
-                                 <Text> Enter Button </Text>
-                               </View>
-                   </TouchableOpacity>
-
-          </View>
-
-
-      );
-
-      }
-
-  }
-  const styles = StyleSheet.create({
-  Gui: {
-    flexDirection: 'column',
-    padding: 10,
-    flex:1,
-    backgroundColor: "lightblue",
-    //justifyContent: "space-around",
-   // alignItems: "center",
-    },
-
-    textBox: {
-
-
-       height: 50,
-       width: 300,
-     backgroundColor: 'gray'
-    },
-    button1: {
-    height: 30,
-    width: 30,
-    backgroundColor: 'yellow',
-    borderRadius: 50
-
-    },
-    button2: {
-        height: 30,
-        width: 30,
-        backgroundColor: 'gray',
-
-
-        },
-    layout2: {
-
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    width: 420,
-    height: 60,
-    position: 'absolute',
-    top: 30,
-    backgroundColor: 'pink'
-
-    },
-    row2: {
-
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    width: 370,
-    height: 55,
-
-
-
-    },
-    textIn: {
-    bottom: 200,
-    position: 'absolute',
-    width: 400,
-    height:60,
-    backgroundColor: 'gray',
-    paddingLeft: 10
-    },
-    enter: {
-    flex: 1,
-     width: 100,
-     height: 60,
-     position: 'absolute',
-     bottom: 20,
-     backgroundColor: 'green',
-
-
+    onStarRatingPress(rating) {
+        this.setState({
+        starCount: rating
+     });
     }
+     onEnterClick(){
+
+          let userReview =
+          {
+            comment: this.state.text,
+            rating: this.state.starCount,
+          }
+          this.props.addReview(userReview);
+
+         }
+    render() {
+    {winHeight, winWidth} ;
+        return (
+         <View style = {styles.Gui}   >
+
+                           <View style = {styles.firstLine} >
+                           <Text style = {{fontSize: 20}} > Review your experience at: {"\n"} [insert place name here] </Text>
+                           </View>
+
+                             <ScrollView  keyboardShouldPersistTaps = {"never"} style = {styles.commentBox}>
+                                                     <KeyboardAvoidingView  behavior = "padding" enabled>
+                                                              <View style = {styles.textIn}>
+                                                                <TextInput  placeholder = {'Leave a comment...'}  onChangeText={(text) => this.setState({text})}
+                                                                  value = {this.state.text}/>
+                                                               </View>
+                                                      </KeyboardAvoidingView>
+                                                       </ScrollView>
+
+                         <View style = {styles.stars}>
+                            <Text style = {styles.starText} > Rate your experience: </Text>
+                              <StarRating
+                                         disabled={false}
+                                         maxStars={5}
+                                         rating={this.state.starCount}
+                                         selectedStar={(rating) => this.onStarRatingPress(rating)}
+                                         fullStarColor = {'yellow'}
+                                       />
+                        <Text style = {styles.starText}> {this.state.starCount} stars </Text>
+
+                         </View>
 
 
+                            <View style = {styles.enter}>
+                           <TouchableOpacity onPress = {this.onEnterClick.bind(this)}>
 
-  });
+                                         <Text style = {styles.submitText}> Submit </Text>
+
+                           </TouchableOpacity>
+                           </View>
+
+                  </View>
+
+
+              );
+
+              }
+
+          }
+
+    const styles = StyleSheet.create({
+
+          Gui: {
+            flexDirection: 'column',
+            flex: 1,
+            flexWrap: 'wrap',
+            backgroundColor: 'white',
+            },
+
+          firstLine: {
+            flex: 0.5,
+            flexShrink: 2,
+            top: 10,
+            bottom: 20,
+            height: 70,
+            alignContent: 'flex-start',
+            overflow: 'scroll',
+            },
+
+          commentBox: {
+            top: 10,
+            flex: 2.5,
+            height: 150,
+            width: winWidth,
+            overflow: 'scroll',
+
+            },
+         textIn: {
+            top: 10,
+            flex:2.5,
+            borderColor: 'black',
+            borderWidth: 1.5,
+            width: winWidth,
+            height:150,
+            paddingLeft: 10,
+            overflow: 'scroll',
+            },
+
+         stars: {
+            flex: 1.5,
+            width: 300,
+            height: 60,
+            alignSelf: 'center',
+            overflow: 'hidden',
+            },
+
+
+        enter: {
+             height: 60,
+             bottom:20,
+             backgroundColor: 'limegreen',
+             width: winWidth,
+            },
+
+        starText: {
+            fontSize: 24,
+            color: 'black',
+            fontWeight: 'bold',
+            textAlign: 'center',
+            lineHeight: 60,
+            textAlignVertical: 'center',
+            },
+
+        submitText: {
+            fontSize: 24,
+            color: 'white',
+            fontWeight: 'bold',
+            textAlign: 'center',
+            lineHeight: 60,
+            textAlignVertical: 'center',
+            },
+
+         textBox: {
+            flex:1,
+            height: 70,
+            width: winWidth,
+             backgroundColor: 'gray',
+             borderColor: 'black',
+             borderWidth: 10,
+             top: 110,
+            },
+
+         button2: {
+                height: 30,
+                width: 30,
+                backgroundColor: 'white',
+                borderColor: 'black',
+                },
+          });
