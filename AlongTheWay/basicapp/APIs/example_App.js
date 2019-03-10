5376bdf0d75f0d496303d863a7e1763de0cadf2a
avery <sfost13@lsu.edu>
2019-03-10 13:50:19 -0500

    Update example_App.js

--- AlongTheWay/basicapp/APIs/example_App.js
+++ AlongTheWay/basicapp/APIs/example_App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View,TouchableOpacity} from 'react-native';
-
 import Foursquare from './basicapp/APIs/Foursquare/Foursquare';
 import Google from './basicapp/APIs/Google/Google';
 import Yelp from './basicapp/APIs/Yelp/Yelp';
@@ -24,39 +23,64 @@ export default class App extends Component {
        async hi(){
 
                    //some params notation differs slightly depending on provider, website has list of available params
-             foursquare.setParams({
-                                    ll: "30.414175,-91.186256",
-                                    limit: "1",
-                                    radius: "1000",
-                                    intent:"browse"
-                                    });
-              google.setParams({
-                                    location: "30.414175,-91.186256",
-                                    type: "restaurant",
-                                    keyword: "coffee",
-                                    radius: "1000"
-                  });
-              yelp.setParams({
-                                    latitude: "30.414175",
-                                    longitude: "-91.186256",
-                                    term: "food",
-                                    radius: "1000"
-                             });
-               //returns info for given place by its id (no params needed atm)
-               //each id given for a result cant be used for another api(i think) ex. a venueId from foursquare wont work for yelp or google
-                let data1 = await yelp.getDetails("rdjLjIlAjaZ2rxmWf-V5HQ");
-                console.log(data1);
+                         foursquare.setParams({
+                                                ll: "30.414175,-91.186256",
+                                                limit: "5",
+                                                radius: "1000",
+                                                query:"pizza"
+                                                });
+                          google.setParams({
+                                                location: "30.414175,-91.186256",
+                                                type: "restaurant",
+                                                keyword: "pizza",
+                                                radius: "1000"
+                              });
+                          yelp.setParams({
+                                                latitude: "30.414175",
+                                                longitude: "-91.186256",
+                                                term: "pizza",
+                                                radius: "1000",
+                                                limit: 5
+                                         });
+
+                            //**Comment out some of these so that the console isn't as hard to read**\\
 
 
-               //returns results of search based of given params
+                           //returns info for given place by its id (no params needed atm)
+                           //each id given for a result cant be used for another api(i think) ex. a venueId from foursquare wont work for yelp or google
+                               //details for little caesars on Highland
+                            console.log("------------------------------------------")
+                            let data1 = await google.getDetails("ChIJzTN-vSinJoYRRPtwbgRHuJo");
+                            console.log(data1);
 
-                let data2 = await foursquare.search();
-                console.log(data2);
 
-              //Comment out some of these so that the console isn't as hard to read
+                            //returns results of search based of given params
+                            console.log("------------------------------------------")
+                            let data2 = await google.search();
+                            console.log(data2);
 
-                let data3 = await google.getDetails("ChIJdU4_JianJoYReG7XAV8wXP0");
-                console.log(data3);
+
+                             //details for little caesars on Highland
+                           console.log("------------------------------------------")
+                            let data3 = await foursquare.getDetails("4d92867a14228cfa53e14e7b");
+                             console.log(data3);
+
+                             //returns results of search based of given params
+                           console.log("------------------------------------------")
+                            let data4 = await foursquare.search();
+                             console.log(data4);
+
+                            //details for little caesars on Highland
+                           console.log("------------------------------------------")
+                            let data5 = await yelp.getDetails("qD45jQgzuQAqA1fAYHFOuA");
+                            console.log(data5);
+
+                            //returns results of search based of given params
+                           console.log("------------------------------------------")
+                            let data6 = await yelp.search();
+                            console.log(data6);
+
+                           console.log("------------------------------------------")
   }
 
   render() {
