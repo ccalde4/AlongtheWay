31fa8375d1e11443bd0db10816d89b318ce171c3
Clauida Caldera <ccalde4@lsu.edu>
2019-04-07 20:41:35 -0500

    Create handlePress in Map.js

--- AlongTheWay/basicapp/Components/Home/Main/Map/Map.js
+++ AlongTheWay/basicapp/Components/Home/Main/Map/Map.js
@@ -40,16 +40,14 @@ const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
    return false;
 
    }
-   handlePress(pressedMarker){
-   this.setState((previousState) => ({markerClicked: !previousState.markerClicked}))
-   this.props.onMarkerClicked(pressedMarker);
-   <MarkerPopup location = {pressedMarker.response.venue.location}
-                                  name = {pressedMarker.response.venue.name}
-                                  id = {pressedMarker.response.venue.id}
-                                  contact = {pressedMarker.response.venue.contact}
-                                  rating = {pressedMarker.response.venue.rating}/>
+  handlePress(index){
 
-   }
+       this.props.onMarkerClick(index);
+      
+
+
+       }
+
    fitMap(){
    this.map.fitToCoordinates(this.props.polyline);
    }
