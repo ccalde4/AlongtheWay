4c3b675388f470bdbc7d7e330c3e235f28d2fb41
sarah <sbroom7@lsu.edu>
2019-04-23 11:44:05 -0400

    comments
    code review comments

--- AlongTheWay/basicapp/utils/MarkerSort.js
+++ AlongTheWay/basicapp/utils/MarkerSort.js
@@ -15,11 +15,7 @@ constructor(s,b){
 
 
 }
-    //**WARNING POTENTIAL PROBLEMS**\\
-    //manipulating the array unsorted is derived from might possibly cause problems if unsorted is only pointing to the objects
-    //of original array
-    //consider cloning TopPlaces or sorted when return if this is the case
-    //*note* this quicksort does not manipulate the original array; other possible side effects haven't been checked for yet
+  //creates toSort array consisting of all places from yelp API within set radius
   sort(toSort){
 
        for(let i =0; i < toSort.length; i++){
@@ -33,30 +29,34 @@ constructor(s,b){
 
 
 
-
+//sorting algorithm to sort unsorted array of places by their rating
 quickSort(unsorted, left, right){
-
-
     if(left >= right)return;
+    //pivot set to middle element in array and set to compare based upon rating
     let pivot = (unsorted[Math.floor((left + right) / 2)][sortBy]);
+    //
     let index = this.partition(unsorted, left, right, pivot);
+    //recursively sorting elements before pivot
     this.quickSort(unsorted, left, index -1);
+    //recursively sorting elements after pivot
     this.quickSort(unsorted, index, right);
-
+    //returning unsorted array that has been sorted by rating
     return unsorted;
 
 }
 
+//partitions elements into two sub-arrays(left and right of the pivot)
 partition(unsorted, left, right, pivot){
     while(left <= right){
-
+        //while value at left is less than pivot move left
         while(unsorted[left][sortBy] < pivot){
             left++;
         }
-
+        //while value at right is greater than pivot move right
         while(unsorted[right][sortBy] > pivot){
             right--;
         }
+        //if sub-arrays do no match swap left and right 
         if(left <= right){
 
             [unsorted[left], unsorted[right]] = [unsorted[right], unsorted[left]];
@@ -73,6 +73,7 @@ partition(unsorted, left, right, pivot){
       return topPlaces;
     }
 
+    //creating new array of top elements from sorted array
     setTopPlaces(sorted){
         let i = sorted.length-1;
         let j = 0;
